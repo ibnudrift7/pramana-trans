2a2463dc14af7d472641863d55fbe48c05d7590a
ibnudrift <ibnudrift@gmail.com>
2025-05-14 15:23:44 +0700

    ff

--- includes/_header.php
+++ includes/_header.php
@@ -1,5 +1,5 @@
 <header id="header" class="header sticky-top">
-    <div class="topbar d-flex align-items-center">
+    <div class="topbar d-flex align-items-center d-none">
         <div class="container d-flex justify-content-center justify-content-md-between">
             <div class="contact-info d-flex align-items-center">
                 <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:info@pramanatrans.com">info@pramanatrans.com</a></i>
@@ -20,7 +20,7 @@
         <div class="container position-relative d-flex align-items-center justify-content-between">
             <a href="index.php" class="logo d-flex align-items-center">
                 <img src="assets/img/logo-hd.png" alt="">
-                <h1 class="sitename">Pramana Trans</h1>
+                <h1 class="sitename text-gold">Pramana Trans</h1>
             </a>
 
             <nav id="navmenu" class="navmenu">
@@ -29,6 +29,11 @@
                     <li><a href="sewa-mobil.php">Sewa Mobil</a></li>
                     <li><a href="tour.php">Paket Tour</a></li>
                     <li><a href="blog.php">Blog</a></li>
+                    <li class="d-flex align-items-center">
+                        <!-- EN ID -->
+                        <a href="#" class="language-id"><span>ID</span></a>
+                        <a href="#" class="language-en ms-2"><span>EN</span></a>
+                    </li>
                 </ul>
                 <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
             </nav>
